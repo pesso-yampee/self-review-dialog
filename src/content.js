3b4bf2766d34a1918f9d06ee9297f1cd1bbc9418
yamasaki junpei <opss0622@gmail.com>
2024-05-19 10:39:57 +0900

    update: DOMの指定方法を修正

--- src/content.js
+++ src/content.js
@@ -1,5 +1,5 @@
 window.addEventListener('load', () => {
-  const createPRButton = document.querySelector('button[data-ga-click*="Create pull request"]');
+  const createPRButton = document.querySelector('button[data-hydro-click*="pull_request"]');
   if (createPRButton) {
     createPRButton.addEventListener('click', () => {
       alert('セルフレビューしましたか？');
